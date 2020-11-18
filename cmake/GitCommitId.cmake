aa54f251785671fa7f27755ba847a02b4332b229
klx99 <xiaokun.meng@qcast.cn>
2020-11-18 14:43:07 +0800

    CU-55x57q fix cmake build version to commit-id issue.

---
+++ cmake/GitCommitId.cmake
@@ -0,0 +1,17 @@
+if(__feeds_git_commit_id_included)
+    return()
+endif()
+set(__feeds_git_commit_id_included TRUE)
+
+function(git_head_commit_id _commit_id)
+    find_package(Git)
+    execute_process(
+        COMMAND "${GIT_EXECUTABLE}" rev-parse --short HEAD
+        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+        OUTPUT_VARIABLE HEAD_COMMIT_ID)
+    if(NOT HEAD_COMMIT_ID)
+        set(HEAD_COMMIT_ID "0000000")
+    endif()
+    string(SUBSTRING ${HEAD_COMMIT_ID} 0 7 HEAD_COMMIT_ID)
+    set(${_commit_id} ${HEAD_COMMIT_ID} PARENT_SCOPE)
+endfunction()
