2dd5e6cee51fc544c33ea50acaae16080a9a0240
klx99 <xiaokun.meng@qcast.cn>
2020-12-08 19:12:15 +0800

    CU-5fr16t fix msgqueue conflic issue.

--- src/common/Log.hpp
+++ src/common/Log.hpp
@@ -32,7 +32,6 @@ private:
   static void Print(int level, const char* tag, const char* format, va_list ap);
   static const char* ConvColor(int level);
 
-  static std::mutex Mutex;
   /*** class function and variable ***/
   explicit Log() = delete;
   virtual ~Log() = delete;
